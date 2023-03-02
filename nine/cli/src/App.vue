01e486796ea3feab065a5e653ab4473459d7baa1
jhleeon <jhleon288@gmail.com>
2023-03-02 13:44:08 +0600

    add cli

---
+++ nine/cli/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data() {
+    return {
+      msg: "Hello Vue Developer",
+    }
+  }
+}
+
+</script>
